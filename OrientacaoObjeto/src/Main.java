6a1e63efa94324cef94d32636c0e8ba746ea1a58
aluno <aluno@DSN1251017049>
2022-04-11 11:27:34 -0300

    Orientacao e Objeto

---
+++ OrientacaoObjeto/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+public static void Main(String[]args) {
+	Pessoa pessoa1 = new Pessoa();//Criando a variavel e instanciando o objeto pessoa1//
+	
+	pessoa1.nome="Antonio";
+	pessoa1.numFigurinhas=1;
+	
+	pessoa1.receber(3);
+	
+	System.out.println(pessoa1.nome);
+	System.out.println(pessoa1.numFigurinhas);
+
+
+	
+		
+	}
+
+	 
+		
+	}
+	
+
+
---
+++ OrientacaoObjeto/src/Pessoa.java
@@ -0,0 +1,14 @@
+
+public class Pessoa {
+	String nome;
+	int numFigurinhas;
+	
+	void receber(int numFigurinhas) {
+		this.numFigurinhas += numFigurinhas ;
+	}
+
+	void dar () {
+		// TODO Auto-generated constructor stub
+	}
+
+}
