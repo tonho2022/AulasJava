42210cf89c31aef150795efe9824e84c3ec8e627
aluno <aluno@10.107.71.36>
2022-04-14 11:23:33 -0300

    primeiro commit

--- OrientacaoObjeto/src/Pessoa.java
+++ OrientacaoObjeto/src/Pessoa.java
@@ -1,14 +1,29 @@
 
 public class Pessoa {
+
 	String nome;
 	int numFigurinhas;
-	
-	void receber(int numFigurinhas) {
-		this.numFigurinhas += numFigurinhas ;
+
+	void receber(int numfigurinhas) {
+		this.numFigurinhas += numfigurinhas;
 	}
 
-	void dar () {
-		// TODO Auto-generated constructor stub
+	void dar (int numFigurinhas, Pessoa pessoa) {
+		
+		boolean dar(int numFigurinhas, Pessoa pessoa)
+		
+		
+	if(this.numFigurinhas<numFigurinhas) {
+		System.out.println("Você não tem a quantidade nescessaria de figurinhas ");
+		return false;
+				
+		
 	}
-
+	else {
+	this.numFigurinhas -= numFigurinhas;
+	pessoa.receber(numFigurinhas);
+	return true;
+	}
+	}
 }
+
---
+++ OrientacaoObjeto/src/main.java
@@ -0,0 +1,47 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		Pessoa pessoa1 = new Pessoa();//Criando a variavel e instanciando o objeto pessoa1//
+		System.out.println(pessoa1);
+		
+		pessoa1.nome="Antonio";
+		pessoa1.numFigurinhas=1;
+		
+		pessoa1.receber(3);
+		
+		System.out.println(pessoa1.nome);
+		System.out.println(pessoa1.numFigurinhas);
+		
+		Pessoa pessoa2= new Pessoa();
+		
+		
+		pessoa2.nome="Giovana";
+		pessoa2.numFigurinhas=3;
+		
+		
+		System.out.println(pessoa2.nome);
+		System.out.println(pessoa2.numFigurinhas);
+		
+		
+	
+		pessoa1.dar(5, pessoa2);
+		System.out.println("depois");
+		
+		System.out.println(pessoa1.nome);
+		System.out.println(pessoa1.numFigurinhas);
+		
+		System.out.println(pessoa2.nome);
+		System.out.println(pessoa2.numFigurinhas);
+		
+		
+				
+				
+		
+		
+		
+		
+		
+	}
+
+}
