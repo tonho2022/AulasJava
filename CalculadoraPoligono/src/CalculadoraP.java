5b8c2fa893887c4bafcdcbe1f1b29713d6dc2339
aluno <aluno@DSN1251017049>
2022-04-18 11:04:30 -0300

    Calculadora da area de poligonos

---
+++ CalculadoraPoligono/src/CalculadoraP.java
@@ -0,0 +1,14 @@
+
+public class CalculadoraP {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		//metodo calcular area 
+		
+		System.out.println("Bem vindo a calculadora da area de poligonos");
+		double x = scanner.nextIn
+
+	}
+
+}
