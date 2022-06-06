4efe6e9d864077d2dc977f0e30cd922546ed4498
tonho2022 <102963010+tonho2022@users.noreply.github.com>
2022-06-06 09:40:24 -0300

    Update Aplicacao.java

--- ClasseAbstrata/src/Aplicacao.java
+++ ClasseAbstrata/src/Aplicacao.java
@@ -3,14 +3,14 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 	
-	//	Item item = new Item(); Não tem como instanciar pq é de uma classe abstrata
+	//	Item item = new Item(); NÃ£o tem como instanciar pq Ã© de uma classe abstrata
 		
-		Item item= new Cogumelo();
+		Item item= new Cogumelo();//instanciando o objeto cogumelo e criei uma variavel item que sera usada para istanciar o resto dos objetos
 		item.pegar();
 		
 		item = new Moeda();
 		item.pegar();
-		
+		// instanciar o objeto moeda e o objeto diamante
 		item = new Diamante();
 		item.pegar();
 
