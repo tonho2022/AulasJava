d5439aacb6297733cbd19605bd1fef28012b364c
tonho2022 <102963010+tonho2022@users.noreply.github.com>
2022-06-06 09:45:11 -0300

    Update Moeda.java

--- ClasseAbstrata/src/Moeda.java
+++ ClasseAbstrata/src/Moeda.java
@@ -2,6 +2,8 @@
 public class Moeda extends Item{
 
 	@Override
+	//puxando o metodo pegar da classe item usando o override 
+	//syso para mostrar a mensagem na tela quando o metodo for executado
 	public void pegar() {
 	System.out.println("Pegou Moeda");
 	}
