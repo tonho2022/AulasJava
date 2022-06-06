0541ab75f29cff87332efc1959d91f60ecf4edfd
tonho2022 <102963010+tonho2022@users.noreply.github.com>
2022-06-06 09:53:56 -0300

    Update Item.java

--- ClasseAbstrata/src/Item.java
+++ ClasseAbstrata/src/Item.java
@@ -1,17 +1,17 @@
 
 public abstract class Item {
 
-	private int posX;
+	private int posX; //criando variaveis privadas
 	private int posY;
 	
-	public abstract void pegar();
+	public abstract void pegar();//colocando metodo da classe abstrata pois ela esta sendo extendida
 		
 		
 	public int getPosX() {
 		return posX;
 	}
 
-	public void setPosX(int posX) {
+	public void setPosX(int posX) { //Usando os metodos getters e setters o metodo getter serve para retornar o valor da variavel e o setter para definir ou atualizar o valor
 		this.posX = posX;
 	}
 
