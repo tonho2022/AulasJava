befbb96430fc25d59e965a1740560bba560464af
tonho2022 <102963010+tonho2022@users.noreply.github.com>
2022-06-06 10:12:55 -0300

    Update DiaDaSemana.java

--- Enum/src/DiaDaSemana.java
+++ Enum/src/DiaDaSemana.java
@@ -1,6 +1,6 @@
 
 public enum DiaDaSemana {
-
+//lista de enums que recebem um valor inteiro
 	DOMINGO(1), 
 	SEGUNDA(2),
 	TERCA(3), // Enums podem receber metodos e atributos
@@ -9,13 +9,13 @@ public enum DiaDaSemana {
 	SEXTA(6),
 	SABADO(7);
 	
-	private int num;
+	private int num;//variavel para o valor que os dias irão receber
 
 	private DiaDaSemana(int num) {
 		this.num = num;
 	}
 
-	public int getNum() {
+	public int getNum() {//puxando construtor privado
 		return num;
 	}
 	
