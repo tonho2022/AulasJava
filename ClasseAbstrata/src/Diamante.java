d05298c751a96b507472c81f306b1c003b736cfb
tonho2022 <102963010+tonho2022@users.noreply.github.com>
2022-06-06 09:44:06 -0300

    Update Diamante.java

--- ClasseAbstrata/src/Diamante.java
+++ ClasseAbstrata/src/Diamante.java
@@ -2,9 +2,10 @@
 public class Diamante extends Item  {
 
 	@Override
-	//puxando o metodo pegar da classe item usando o override
+	//puxando o metodo pegar da classe item usando o override 
 	public void pegar() {
 	System.out.println("Pegou Diamante");
+		//syso para mostrar a mensagem na tela quando o metodo for executado
 	}
 
 }
