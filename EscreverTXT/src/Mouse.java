35d21cb10212a83f515b82aa98f06844ef14dfe5
tonho2022 <102963010+tonho2022@users.noreply.github.com>
2022-06-06 10:22:41 -0300

    Update Mouse.java

--- EscreverTXT/src/Mouse.java
+++ EscreverTXT/src/Mouse.java
@@ -1,19 +1,19 @@
 
 public class Mouse {
  
-	private boolean funciona;
+	private boolean funciona; //criando os atributos do meu objeto
 	private int qtdBotoes;
 	private String tamanho;
 	
 	
 	public Mouse(boolean funciona, int qtdBotoes, String tamanho) {
 		this.funciona = funciona;
-		this.qtdBotoes = qtdBotoes;
+		this.qtdBotoes = qtdBotoes;//instanciando variaveis para receber as informações
 		this.tamanho = tamanho;
 	}
 	@Override
 	public String toString() {
-		return "Mouse [funciona=" + funciona + ", qtdBotoes=" + qtdBotoes + ", tamanho=" + tamanho +  "]" + "\n";
+		return "Mouse [funciona=" + funciona + ", qtdBotoes=" + qtdBotoes + ", tamanho=" + tamanho +  "]" + "\n";//override com to string para puxar esse metodo desta classe
 	}
 	public boolean isFunciona() {
 		return funciona;
@@ -22,7 +22,7 @@ public class Mouse {
 		this.funciona = funciona;
 	}
 	public int getQtdBotoes() {
-		return qtdBotoes;
+		return qtdBotoes;                                       //getters and setters dos valores do nosso objeto
 	}
 	public void setQtdBotoes(int qtdBotoes) {
 		this.qtdBotoes = qtdBotoes;
