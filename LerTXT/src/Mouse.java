77a71c358f0737a6d5ac39c96a3a16144b4af38a
tonho2022 <102963010+tonho2022@users.noreply.github.com>
2022-06-12 16:18:22 -0300

    Update Mouse.java

--- LerTXT/src/Mouse.java
+++ LerTXT/src/Mouse.java
@@ -1,22 +1,22 @@
 public class Mouse {
  
-	private boolean funciona;
+	private boolean funciona;//criando variaveis privadas
 	private int qtdBotoes;
 	private String tamanho;
 	
 	
 	public Mouse(boolean funciona, int qtdBotoes, String tamanho) {
 		this.funciona = funciona;
-		this.qtdBotoes = qtdBotoes;
+		this.qtdBotoes = qtdBotoes;//criando atributos do objeto mouse
 		this.tamanho = tamanho;
 	}
 	@Override
-	public String toString() {
+	public String toString() {//override no to metodo to string 
 		return "Mouse [funciona=" + funciona + ", qtdBotoes=" + qtdBotoes + ", tamanho=" + tamanho +  "]" + "\n";
 	}
 	public Mouse (String texto) {
 		String[]arrayAuxiliar1= texto.split(",");
-		String[]arrayAuxiliar2 = arrayAuxiliar[0].split("=");
+		String[]arrayAuxiliar2 = arrayAuxiliar[0].split("=");//array para repetir os metodos 
 		this.funciona=arrayAuxiliar[1].trim();
 		
 		String[] arrayAuxiliar3=arrayAuxiliar1[1].split("=");
@@ -26,7 +26,7 @@ public class Mouse {
 		this.tamanho = arrayAuxiliar4[1].trim();
 		
 		}
-	
+	//puxando metodos getters and setters para acessar as variaveis privadas
 	public boolean isFunciona() {
 		return funciona;
 	}
