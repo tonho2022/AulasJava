724a32b4749ba5ee54901825b7de8cf5e3705131
tonho2022 <102963010+tonho2022@users.noreply.github.com>
2022-06-12 16:01:10 -0300

    Update Apartamento.java

--- Interfaces/src/Apartamento.java
+++ Interfaces/src/Apartamento.java
@@ -2,7 +2,7 @@
 public class Apartamento implements ItemCaro{
 
 	@Override
-	public double getPreco() {
+	public double getPreco() {//metodo override para acessar um metodo que ja existe em outras classes
 		
 		return 250000;
 	}
