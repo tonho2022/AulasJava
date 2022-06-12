1a19e2fe1a2d55e5a512c379ecc28efe91966157
tonho2022 <102963010+tonho2022@users.noreply.github.com>
2022-06-12 16:25:35 -0300

    Update Cachorro.java

--- Polimorfismo/src/Cachorro.java
+++ Polimorfismo/src/Cachorro.java
@@ -1,11 +1,11 @@
 
-public class Cachorro extends Animal{
+public class Cachorro extends Animal{//usando extends para herdar da superclasse animal
 
 	@Override
-	public void falar() {
+	public void falar() {//override do metodo falar que ja existe em outra classe
 		System.out.println("AUAU");
 	}
-	public void Morder() {
+	public void Morder() {//criando metodo para a classe cachorro
 		System.out.println("NHAC");
 	}
 
