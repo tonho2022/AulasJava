d9211a8f2463902f95b14fb6e8ed2077c89c50a4
tonho2022 <102963010+tonho2022@users.noreply.github.com>
2022-06-12 16:27:57 -0300

    Update Gato.java

--- Polimorfismo/src/Gato.java
+++ Polimorfismo/src/Gato.java
@@ -1,8 +1,8 @@
 
-public class Gato extends Animal {
+public class Gato extends Animal {//extends pra classe gato herdar a superclasse animal
 
 	@Override
-	public void falar() {
+	public void falar() {//override do metodo falar pois ele foi criado em outra classe
 		
 		System.out.println("MIAUU");
 	}
