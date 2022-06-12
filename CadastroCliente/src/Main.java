14bcfb423a60fa152eab32e31f24300f9e9c2337
tonho2022 <102963010+tonho2022@users.noreply.github.com>
2022-06-12 15:18:03 -0300

    Update Main.java

--- CadastroCliente/src/Main.java
+++ CadastroCliente/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static final double PI = 3.14;
 	
 	private enum Sexo{
-		MASCULINO,FEMININO;
+		MASCULINO,FEMININO;//criando um enum privado
 
 		char[] getNum() {
 			
@@ -23,15 +23,15 @@ public class Main {
 		Sexo sexo = Sexo.FEMININO;
 		 sexo = Sexo.MASCULINO;
 		
-		String[] arrayInt = new String [5];
+		String[] arrayInt = new String [5];//criando um array 
 		String[] ArrayString = new String [4];
 		double[] ArrayDouble = new double[3];
 		
 		arrayInt[3] = MASCULINO;
 		int[] numero = new int [6];
-		Scanner teclado = new Scanner(System.in);
+		Scanner teclado = new Scanner(System.in);//istanciando o scanner para receber valores do usuario
 
-		for (int aux2 = 0; aux2 < numero.length; aux2++) {
+		for (int aux2 = 0; aux2 < numero.length; aux2++) {//colocando o for para fazer a repetição até o valor colocado no conchete
 			System.out.println("Digite 1 para realizar o cadastro");
 			numero[aux2] = teclado.nextInt();
 		}
