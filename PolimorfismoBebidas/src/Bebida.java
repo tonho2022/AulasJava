3d982b7fa994ffdeea30e59220f877a6226e9a19
tonho2022 <102963010+tonho2022@users.noreply.github.com>
2022-06-12 16:35:57 -0300

    Update Bebida.java

--- PolimorfismoBebidas/src/Bebida.java
+++ PolimorfismoBebidas/src/Bebida.java
@@ -1,20 +1,20 @@
 
 public class Bebida {
-	
+	//criando variaveis privadas
 	private String nome;
 	private boolean aquecer;
-	public Bebida(String nome, boolean aquecer) {
+	public Bebida(String nome, boolean aquecer) {//criando um construtor
 		super();
 		this.nome = nome;
 		this.aquecer = aquecer;
 		
 	}
 	
-	public void preparar() {
+	public void preparar() {//criando metodo preparar que não vai retornar
 		System.out.println("Pegando copo");	
 	
 	}
-	
+	//metodos getters and setters para acessar os valores 
 	public String getNome() {
 		return nome;
 	}
