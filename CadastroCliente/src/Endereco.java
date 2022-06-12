e1a16a7d92c0255ef3883e0f87a19d1c96885dc1
tonho2022 <102963010+tonho2022@users.noreply.github.com>
2022-06-12 15:22:29 -0300

    Update Endereco.java

--- CadastroCliente/src/Endereco.java
+++ CadastroCliente/src/Endereco.java
@@ -1,8 +1,50 @@
 
 public class Endereco {
+	
+	//Criando variaveis com atributos privados
+	private int num;
+	private String rua;
+	private String bairro;
 
-	String Rua;
-	int Num;
+public Endereco(String[] ArrayLine) {
+		
+	//colocando as arrays para receber os locais dos valores
+		String[] ArrayRua = ArrayLine[3].split("=");
+		this.rua = ArrayRua[1].trim();
+				
+		String[] ArrayNumero = ArrayLine[4].split("=");
+		this.numero = Integer.parseInt(ArrayNumero[1].trim());
+		
+		String[] ArrayBairro = ArrayLine[5].split("=");
+		this.bairro = ArrayBairro[1].trim();
+	}
+
+	//Colocando gettes and setters para acessar os valores das variaveis privadas
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	
+	@Override
+	public String toString() {
+		return "N°" + num + ", Rua:" + rua + ", Bairro:" + bairro;
+	}
+
+	
 	
 	
 }
